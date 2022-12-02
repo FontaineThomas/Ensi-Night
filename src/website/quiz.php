102eb1ac6b7517f6e39e89af29addd27a08e9a5d
Thibaut <hertzogthibaut@gmail.com>
2022-12-02 04:22:57 +0100

    Ajout de la pagequiz + imgs

---
+++ src/website/quiz.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Face aux MST</title>
+    <link rel="icon" href="VIH.png">
+    <!-- Css -->
+    <link href="assets/css/plugins/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/fonts/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/fonts/bs-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/css/plugins/slick.css" rel="stylesheet">
+    <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
+    <link href="assets/css/plugins/animate.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <link href="assets/css/game.css" rel="stylesheet">
+</head>
+
+<body>
+
+<section class="section_game section-bg">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="game_card deck">
+                <div class="content">
+                    <div class="front"></div>
+                    <div class="back">
+                        Front
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="score">
+    <div class="center draw_card">
+        <input type="submit" class="btn btn-dark center_button" value="Tirer une carte" name="draw">
+    </div>
+    <div class="top-left player_score">
+        <h6>JOUEUR 1</h6>
+        <h6>00</h6>
+    </div>
+    <div class="top-right player_score">
+        <h6>JOUEUR 2</h6>
+        <h6>00</h6>
+    </div>
+    <div class="bottom-left player_score">
+        <h6>JOUEUR 3</h6>
+        <h6>00</h6>
+    </div>
+    <div class="bottom-right player_score">
+        <h6>JOUEUR 4</h6>
+        <h6>00</h6>
+    </div>
+</div>
+
+<table class="response-tab">
+    <tr>
+        <td>
+            <input type="submit" class="btn btn-dark responses" value="Reponse" name="reponse">
+        </td>
+        <td>
+            <input type="submit" class="btn btn-dark responses" value="Reponse" name="reponse">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="submit" class="btn btn-dark responses" value="Reponse" name="reponse">
+        </td>
+        <td>
+            <input type="submit" class="btn btn-dark responses" value="Reponse" name="reponse">
+        </td>
+    </tr>
+</table>
+
+<div class="df-cursor"></div>
+<div class="df-cursor2"></div>
+<!-- Scripts -->
+<script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
+<script src="assets/js/plugins/bootstrap.bundle.min.js"></script>
+<script src="assets/js/plugins/particles.js"></script>
+<script src="assets/js/plugins/slick.min.js"></script>
+<script src="assets/js/plugins/slick-animation.min.js"></script>
+<script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/plugins/jquery.counterup.min.js"></script>
+<script src="assets/js/plugins/jquery.inview.min.js"></script>
+<script src="assets/js/plugins/cursor.min.js"></script>
+<script src="assets/js/plugins/wow.min.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
